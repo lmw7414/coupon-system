eae4b390de554d275fdba2e6fcc0c95cbf44f967
Lee Min Woo <lmw7414@naver.com>
2024-12-05 16:11:52 +0900

    coupon-api: `synchronized` 로 동시성 문제 해결(좋지 않음)
    synchronized 키워드를 사용하여 동시성 문제를 해결했다.
    
    하지만 synchronized 키워드는 자바 애플리케이션에 종속이 되기 때문에 여러 서버로 확장된다면 lock을 제대로 관리할 수 없게 된다.
     -> 분산락 구현을 통해 해결할 수 있을 듯 하다.

--- coupon-api/src/main/java/org/example/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/org/example/couponapi/service/CouponIssueRequestService.java
@@ -14,7 +14,9 @@ public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        synchronized (this) {
+            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        }
         log.info(String.format("쿠폰 발급 완료. couponId: %s userId: %s", requestDto.couponId(), requestDto.userId()));
     }
 }
--- coupon-core/src/main/java/org/example/couponcore/service/CouponIssueService.java
+++ coupon-core/src/main/java/org/example/couponcore/service/CouponIssueService.java
@@ -28,6 +28,30 @@ public class CouponIssueService {
         saveCouponIssue(couponId, userId);
     }
 
+    /* 이 구조의 문제점
+    @Transactional
+    public void issue(long couponId, long userId) {
+        synchronized (this){
+            Coupon coupon = findCoupon(couponId);
+            coupon.issue();
+            saveCouponIssue(couponId, userId);
+        }
+    }
+    1. 트랜잭션 시작
+    2. lock 획득
+    3. 쿠폰 발급 로직 실행(저장)
+    4. lock 반납
+    5. 트랜잭션 커밋
+    -> lock 반납함과 동시에 다른 쓰레드에서 lock을 획득하기 때문에 동시성 문제가 해결되지 않는다.
+
+    * 해결 방법 *
+    1. lock 획득
+    2. 트랜잭션 시작
+    3. 쿠폰 발급 로직
+    4. 트랜잭션 종료
+    5. lock 반납
+     */
+
     @Transactional(readOnly = true)
     public Coupon findCoupon(long couponId) {
         return couponJpaRepository.findById(couponId)
@@ -45,8 +69,8 @@ public class CouponIssueService {
     }
 
     private void checkAlreadyIssuance(long couponId, long userId) {
-        CouponIssue issue = couponIssueRepository.findFirstCouponIssue(couponId,userId);
-        if(issue != null) {
+        CouponIssue issue = couponIssueRepository.findFirstCouponIssue(couponId, userId);
+        if (issue != null) {
             throw new CouponIssueException(DUPLICATED_COUPON_ISSUE, String.format("이미 발급된 쿠폰입니다. user_id: %s, coupon_id: %s", userId, couponId));
         }
     }
