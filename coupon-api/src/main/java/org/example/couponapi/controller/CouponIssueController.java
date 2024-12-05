e5c1178e2fc8ce60403c3678510cf486feb9e7ec
Lee Min Woo <lmw7414@naver.com>
2024-12-05 15:34:15 +0900

    coupon-api: 쿠폰 발급 api 코드 추가

---
+++ coupon-api/src/main/java/org/example/couponapi/controller/CouponIssueController.java
@@ -0,0 +1,23 @@
+package org.example.couponapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.couponapi.controller.dto.CouponIssueRequestDto;
+import org.example.couponapi.controller.dto.CouponIssueResponseDto;
+import org.example.couponapi.service.CouponIssueRequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CouponIssueController {
+
+    private final CouponIssueRequestService couponIssueRequestService;
+
+    @PostMapping("/v1/issue")
+    public ResponseEntity<CouponIssueResponseDto> issueV1(@RequestBody CouponIssueRequestDto body) {
+        couponIssueRequestService.issueRequestV1(body);
+        return ResponseEntity.ok(new CouponIssueResponseDto(true, null));
+    }
+}
