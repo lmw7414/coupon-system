186ac6ae10269f32b0759c9736581d58df0e74a5
Lee Min Woo <lmw7414@naver.com>
2024-12-07 01:26:02 +0900

    coupon-core: 쿠폰 발급 api 설계 및 검증
    Redis가 싱글쓰레드로 동작하여 동시성 문제가 발생하지 않는다고 하더라도 여러 api 서버를 사용할 때 검증 단계에서 값을 조회하는 과정에서 동시성 문제가 발생할 수 있다. 이 부분을 고려하여 분산락을 사용하여 해결하였다.

--- coupon-core/src/main/java/org/example/couponcore/service/AsyncCouponIssueServiceV1.java
+++ coupon-core/src/main/java/org/example/couponcore/service/AsyncCouponIssueServiceV1.java
@@ -1,16 +1,59 @@
 package org.example.couponcore.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.example.couponcore.component.DistributeLockExecutor;
+import org.example.couponcore.exception.CouponIssueException;
+import org.example.couponcore.model.Coupon;
 import org.example.couponcore.repository.redis.RedisRepository;
+import org.example.couponcore.repository.redis.dto.CouponIssueRequest;
 import org.springframework.stereotype.Service;
+
+import static org.example.couponcore.exception.ErrorCode.*;
+import static org.example.couponcore.util.CouponRedisUtils.getIssueRequestKey;
+import static org.example.couponcore.util.CouponRedisUtils.getIssueRequestQueueKey;
 
 @Service
 @RequiredArgsConstructor
 public class AsyncCouponIssueServiceV1 {
+
     private final RedisRepository redisRepository;
+    private final CouponIssueRedisService couponIssueRedisService;
+    private final CouponIssueService couponIssueService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final DistributeLockExecutor distributeLockExecutor;
 
     public void issue(long couponId, long userId) {
-        String key = "issue.request.sorted_set.couponId=%s".formatted(couponId);
-        redisRepository.zAdd(key, String.valueOf(userId), System.currentTimeMillis());
+        Coupon coupon = couponIssueService.findCoupon(couponId);
+        if(!coupon.availableIssueDate()) {
+            throw new CouponIssueException(INVALID_COUPON_ISSUE_DATE,
+                    String.format("발급 가능한 일자가 아닙니다. couponId: %s issueStart: %s issueEnd: %s", couponId, coupon.getDateIssueStart(), coupon.getDateIssueEnd())
+            );
+        }
+        distributeLockExecutor.execute("lock_%s".formatted(couponId), 3000, 3000, () -> {
+            if(!couponIssueRedisService.availableTotalIssueQuantity(coupon.getTotalQuantity() ,couponId)) {
+                throw new CouponIssueException(INVALID_COUPON_ISSUE_QUANTITY,
+                        String.format("발급 가능한 수량을 초과합니다. couponId: %s userId: %s", couponId, userId)
+                );
+            }
+            if(!couponIssueRedisService.availableUserIssueQuantity(couponId, userId)) {
+                throw new CouponIssueException(DUPLICATED_COUPON_ISSUE,
+                        String.format("이미 발급 요청이 처리되었습니다. couponId: %s userId: %s", couponId, userId)
+                );
+            }
+            issueRequest(couponId, userId);
+        });
+    }
+
+    private void issueRequest(long couponId, long userId) {
+        CouponIssueRequest issueRequest = new CouponIssueRequest(couponId, userId);
+        try {
+            String value = objectMapper.writeValueAsString(issueRequest);
+            redisRepository.sAdd(getIssueRequestKey(couponId), String.valueOf(userId));
+            redisRepository.rPush(getIssueRequestQueueKey(), value);
+        } catch (JsonProcessingException e) {
+            throw new CouponIssueException(FAIL_COUPON_ISSUE_REQUEST, "input: %s".formatted(issueRequest));
+        }
     }
 }
